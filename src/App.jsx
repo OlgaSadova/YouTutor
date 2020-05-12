3c14c8db2b93d2781d90233705076639e6d59431
ValVarius <60993964+ValVarius@users.noreply.github.com>
2020-05-11 19:03:29 -0700

    First page and components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import HomePage from './pages/HomePage';
+function App() {
+  return (
+    <div className="container is-fluid">
+      <div className="notification">
+      <HomePage/>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Hero() {
+    return (
+        <section class="hero is-warning">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Welcome to our Tutor App
+      </h1>
+      <h2 class="subtitle">
+        Let's get some schooling done
+      </h2>
+    </div>
+  </div>
+</section>
+    )
+}
