fccd40a05d7a592cdd132f3139815bb3ba381117
yotamba12 <yotambaram@gmail.com>
2020-05-15 20:12:55 -0700

    filter card

--- src/utils/API.js
+++ src/utils/API.js
@@ -15,6 +15,28 @@ const API = {
     createTeacherPost:function(userData){
         return axios.post(`${BASE_URL}/signup/teacher`,userData,{withCredentials:true})
     },
+    getSkillResult:function(search){
+        return axios.get(`${BASE_URL}/api/searchresult`, search)
+    },
 
 }
 export default API
+
+
+/*
+getTeacherResult:function(search){
+    return axios.get(`${BASE_URL}/api/searchresult`)
+},
+potsTeacherReview:function(id){
+    return axios.post(`${BASE_URL}/api/review/`)
+},
+getTeacherReview:function(id){
+    return axios.get(`${BASE_URL}/api/review/${id}`)
+},
+deleteTeacherReview:function(id){
+    return axios.delete(`${BASE_URL}/api/review/${id}`, {withCredentials:true})
+},
+updatePlayerById:function(id,playerData) {
+    return axios.put(`${BASE_URL}/api/players/${id}`,playerData, {withCredentials:true},)
+},
+*/
