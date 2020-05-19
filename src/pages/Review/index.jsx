1853f088fc9cb109307b775f6bb9d4bf76303bd1
michael Zuniga <michaelzunigaa@gmail.com>
2020-05-19 15:15:17 -0700

    uh

--- src/pages/Review/index.jsx
+++ src/pages/Review/index.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import React,  {useState } from 'react'
+import TeacherCard from '../../components/TeacherCard'
+import StarRatingComponent from 'react-star-rating-component';
+import { useHistory } from 'react-router';
 
-export default function index() {
+export default function Review(props) {
+    const [userReview, setUserReview] = uaeState({
+        rating:"",
+        review:""
+    });
+    
+    const history = useHistory();
+     
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setUserState({
+            ...userState,
+            [name]: value
+        })
+      };
+
+      const handleFormSubmit = event => {
+        event.preventDefault();
+        console.log("inside the handleformsubmit");
+        console.log(userState);
+        
+        
+        API.getReview(userState).then( newReview => {
+            console.log(userState)
+            setUserState({
+                rating:"",
+                review:""
+            })
+
+              API.saveReview(userState.skills)
+              .then(result => {
+                console.log(result)
+              })
+              .catch(err => {
+                console.log(err);
+              })
+            
+
+              
+            
+            
+            history.push("/profile");
+        })
+      
+      }
+
+    
+
+
+   
+
+    return (
+        <div>
+            <TeacherCard 
+
+        </div>
+
+        
+     )
+}
+    
+
+    
+    
     
 
 
@@ -9,9 +75,3 @@ export default function index() {
 
 
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
